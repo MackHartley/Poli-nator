79bd34be8626d8fd53158ca23aee1a92462d2cc2
cmolho <cmolho@macalester.edu>
2016-11-17 14:53:28 -0600

    Set up database models, did first migration, added index view which displays data

--- Poll_R_Bear/views.py
+++ Poll_R_Bear/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Question, Answer
 
 # Create your views here.
+def index(request):
+	questions = Question.objects.all()
+	res = ""
+	for q in questions:
+		answers = Answer.objects.filter(question = q)
+		res += str(q)
+		res = res + '\n\t' + ', '.join([str(a) for a in answers])
+	return HttpResponse(res) 
+
+#def addQuestion(request):
+
+#Useful link: http://django.cowhite.com/blog/working-with-url-get-post-parameters-in-django/
--- Poll_inator/urls.py
+++ Poll_inator/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from Poll_R_Bear import views as bear_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', bear_views.index, name='index'),
 ]
