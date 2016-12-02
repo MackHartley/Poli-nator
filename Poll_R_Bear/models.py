11bdfe6b91d50ed2d2af1c465bc4a4a4a3820f2b
rberman <rberman@macalester.edu>
2016-12-01 22:37:47 -0600

    Added a few questions to the database; edited string returned by question model to return only the question text

--- Poll_R_Bear/models.py
+++ Poll_R_Bear/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
 	text = models.CharField(max_length=200)
 
 	def __str__(self):
-		return 'question: %s' % (self.text)
+		return self.text
 
 class Answer(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
